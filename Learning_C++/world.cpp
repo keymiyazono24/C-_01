5367bfc3f3c7f30986c1eec0c151d1436cc21f8a
keymiyazono24 <keymiyazono24@gmail.com>
2026-09-18 23:48:18 +0530

    Modified files:

---
+++ Learning_C++/world.cpp
@@ -0,0 +1,16 @@
+/**It includes the iostream header file, which is necessary for input and output**/
+#include <iostream> 
+
+int main() {
+    std::cout <<"Hello World! \t"; // It prints "Hello World!" followed by a tab space to the console
+    std::cout << "My name is Key'" <<"\n"; // \n should be inside double quotes to indicate a new line in the string
+    std::cout << 10000 << std::endl ; // It prints the number 10000 to the console followed by a new line
+    std::cout << 4132342 + 2414312 << "\n"; // It prints the result of the addition to the console
+    std::cout << "I like to eat " << "mango" << "\\" <<"banana" << "\n";// It prints "I like to eat mango\banana" to the console, with a backslash before "banana"
+    std::cout << "\""<< "With no powers comes no responsibility" << "\"";// It prints the string "With no powers comes no responsibility" enclosed in double quotes to the console
+    return 0;// It indicates that the program has ended successfully
+}
+
+
+
+
