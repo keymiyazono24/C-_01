f8cb141db2094e7bc6a98db55d2ebe75c12b961b
keymiyazono24 <keymiyazono24@gmail.com>
2026-09-19 00:18:15 +0530

    Updating variables

--- Variables/variables.cpp
+++ Variables/variables.cpp
@@ -1,3 +1,8 @@
 #include <iostream>
 using namespace std;
 
+int speed = 100;// It declares an integer variable named speed and initializes it with the value 100
+int main() {
+cout << "The speed of the car is :" << speed << "km/h";
+return 0;
+}
