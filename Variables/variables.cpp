e5688f96d26af6ea9622de8f3d81e4583379733f
keymiyazono24 <keymiyazono24@gmail.com>
2026-09-19 00:34:29 +0530

    update variables.cpp and variables.exe

--- Variables/variables.cpp
+++ Variables/variables.cpp
@@ -8,6 +8,7 @@ string name = "Key";// It declares a string variable named name and initializes 
 float pi = 3.14;// It declares a float variable named pi and initializes it with the value 3.14
 int main() {
 cout << "The speed of the car is :" << speed << "km/h" <<endl;// It prints the value of the speed variable to the console, along with a message indicating that it is the speed of the car
+cout << pi + speed << endl; // It prints the result of adding the values of the pi and speed variables to the console
 cout << "My name is :" << name << ", the value of pi is :"<< pi << ".Which is "<< myanswer << " and my grade is :"<< grade; // It prints the values of the name, pi, myanswer, and grade variables to the console, along with a message indicating what each value represents
 return 0;
 }
