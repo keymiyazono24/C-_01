d9b498808e9ebeb9a3d5d5c9f73d538fdff562a1
keymiyazono24 <keymiyazono24@gmail.com>
2026-09-20 13:22:52 +0530

    Added files

---
+++ Variables/calculating_areaof_circle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Values
+const float pi =3.14;
+int radius = 5;
+
+// Calculate the area of circle
+int main(){
+    std::cout << "Area of circle is:" << pi * radius * radius;
+    return 0;
+}
---
+++ Variables/constants.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+const std :: string name = " Key"; // constant value cannot be changed\
+
+int main(){
+    std:: cout << name ;
+    return 0;
+}
