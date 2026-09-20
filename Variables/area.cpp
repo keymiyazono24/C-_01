ef1fd18d4c35d1ff5e4bb0caf7023fab29300356
keymiyazono24 <keymiyazono24@gmail.com>
2026-09-20 13:36:25 +0530

    Added area.cpp and area.exe files to calculate the area of a rectangle using variables for length and breadth. The area is computed by multiplying length and breadth, and the result is printed to the console.

---
+++ Variables/area.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//Assigning values
+int Length = 20;
+int Breadth = 30 ;
+//int Area = Length * Breadth;//
+
+// Fn to calculate area of a rectangle 
+int main() {
+std::cout<< " Area of the rectangle is:" << Length * Breadth << "cm" ;
+return 0 ;
+}
